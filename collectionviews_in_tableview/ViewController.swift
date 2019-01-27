521bf99fda33e7b076b8490745042e35ec50dc20
Rian Sanjaya <rian.sj@gmail.com>
2019-01-27 19:45:55 +0700

    Initial Commit

---
+++ collectionviews_in_tableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionviews_in_tableview
+//
+//  Created by Rian Sanjaya Ng on 27/01/19.
+//  Copyright © 2019 Rian Sanjaya Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
